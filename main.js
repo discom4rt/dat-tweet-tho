151d5a13550165270fb74c8f041647b2c2f44a58
Morgan Brown <brown.mhg@gmail.com>
2013-11-05 22:14:22 +0100

    Tweet every 100000 times

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ opts.version('0.0.1')
     .option('-v, --verbose', 'Log some debug info to the console')
     .parse(process.argv);
 
-var TWEET_INTERVAL = 25000,  // The amount of tweets that must be tweeted before one gets fucked
-  count = 0;                 // The number of tweets that have been seen
+var TWEET_INTERVAL = 100000,  // The amount of tweets that must be tweeted before one gets fucked
+  count = 0;                  // The number of tweets that have been seen
 
 // Initialize Twitter API keys
 var twit = new twitter({
